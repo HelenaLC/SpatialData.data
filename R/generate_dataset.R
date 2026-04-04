1b54faf9de2c61a2f69e1ec72602bc8453126a64
Artur-man <artur-man@hotmail.com>
2026-04-04 04:20:28 +0200

    fix doc

--- R/generate_dataset.R
+++ R/generate_dataset.R
@@ -18,6 +18,14 @@
 #' generate_dataset(
 #'   file = zarrfile, 
 #'   sd_version = "0.7.2",
+#'   images = list(
+#'     list(type = "rgb", n_layers = 4L, coordinate_system="global"),
+#'     list(type = "grayscale", n_layers = 1L, coordinate_system="global")
+#'   ),
+#'   labels = list(
+#'     list(n_labels = 12L, n_layers = 4L, coordinate_system="global2"),
+#'     list(n_labels = 12L, n_layers = 0L, coordinate_system="global2")
+#'   ),
 #'   shapes = list(
 #'     list(n_shapes=12L, coordinate_system="global"),
 #'   ),
