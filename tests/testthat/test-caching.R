332e430f20c8beceb37c267fd6967cca7d68703d
Artur-man <artur-man@hotmail.com>
2026-08-17 14:57:53 +0200

    test vig fixes

--- tests/testthat/test-caching.R
+++ tests/testthat/test-caching.R
@@ -8,8 +8,32 @@ test_that("SD.io_available()", {
     expect_true(any(grepl("^(vis|xen)", x)))
 })
 
+path_to_10x_xen_demo <- function(
+    cache=BiocFileCache::BiocFileCache(),
+    zipname="Xenium_V1_human_Breast_2fov_outs.zip", 
+    source = biocOSN_Xenium) {
+  info <- BiocFileCache::bfcquery(cache, zipname)
+  nrec <- nrow(info)
+  if (nrec > 1) {
+    message(sprintf("multiple %s found in cache, using last recorded", zipname))
+  }
+  if (nrec == 1) {
+    message("returning path to cached zip")
+    return(info$rpath[nrec])
+  }
+  fp <- file.path(bucket_path(source), zipname)
+  message(sprintf("retrieving from %s, caching, and returning path", 
+                  bucket_path(source)))
+  BiocFileCache::bfcadd(cache, rname=zipname, fpath=fp, rtype="web")
+}
+
 test_that("SD.io()", {
-    
+  
+    # get dataset
+    zip <- path_to_10x_xen_demo()
+    dir.create(src <- tempfile())
+    unzip(zip, exdir=src)
+
     # directory already exists
     dir.create(out <- tempfile())
     options(sd_version = "0.3.0")
